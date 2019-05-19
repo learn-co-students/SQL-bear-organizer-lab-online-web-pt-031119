4d52f0f2685df961ddaccd67627ac08fd636f5de
Junyoung McArdle <juny.mcardle@gmail.com>
2019-05-19 04:38:13 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "brown", "clam", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "brown", "clam", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "male", "black", "rowdy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "M", "black", "rowdy", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "female", "light brown", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "F", "light brown", "clam", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 6, "male", "dark brown", "bossy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "dark brown", "bossy", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "female", "redish brown", "calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "F", "redish brown", "calm", 0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5, "male", "gray", "grumpy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5, "M", "gray", "grumpy", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "female", "brown", "clam", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "F", "brown", "clam", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 6, "male", "dark brown", "aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 6, "M", "dark brown", "aggressive", 0);
